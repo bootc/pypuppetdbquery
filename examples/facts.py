7e66ee52719cc090e430274572c0e037db33aec2
Chris Boot <bootc@bootc.net>
2016-06-26 23:13:37 +0100

    Correct docstring for the facts example

--- examples/facts.py
+++ examples/facts.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Query for nodes using :mod:`pypuppetdb`.
+Query for selected facts on matching nodes using :mod:`pypuppetdb`.
 """
 
 import pypuppetdb
